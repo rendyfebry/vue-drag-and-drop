841df3bcb807530276fc8203b39f4620524ce4e6
Rendy Febry <rendyfebry@gmail.com>
2020-01-18 23:23:47 +0700

    Basic TodoList structure

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    TodoList,
+  },
 }
 </script>
 
@@ -21,8 +20,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="panel">
+          <h2>To-Do</h2>
+          <div class="list">
+            <div class="task" id="item-1">
+              Item 1
+            </div>
+            <div class="task" id="item-2">
+              Item 2
+            </div>
+            <div class="task" id="item-3">
+              Item 3
+            </div>
+            <div class="task" id="item-4">
+              Item 4
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="panel">
+          <h2>Progress</h2>
+          <div class="list"></div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="panel">
+          <h2>Done</h2>
+          <div class="list"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+}
+</script>
+
+<style scoped>
+.panel {
+  background-color: #f4f5f7;
+  padding: 1em;
+  min-height: 500px;
+}
+
+.panel h2 {
+  font-size: 1.2em;
+  margin-bottom: 1em;
+  text-align: center;
+}
+
+.task {
+  background-color: #ffffff;
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 2px;
+  box-shadow: 0px 1px 2px 0px rgba(9, 30, 66, 0.25);
+}
+</style>
